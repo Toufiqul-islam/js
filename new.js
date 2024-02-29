720b33f29c8e05733549d3938e64c4b25593d012
Toufiqul-islam <toufiqulislam8@gmail.com>
2024-02-29 10:28:06 +0600

    update

---
+++ new.js
@@ -0,0 +1,73 @@
+let a=10 ;
+let b=30;
+/*
+if(a>=95){
+    console.table('A++');
+}else if(a>=80){
+    console.table('A');
+}else if(a>=70){
+    console.table('A');
+}else if(a>=60){
+    console.table('A-');
+}else if(a>=50){
+    console.table('B');
+}else if(a>=40){
+    console.table('C');
+}else if(a>=33){
+    console.table('D');
+}else{
+    console.table('fail');
+}
+*/
+/*
+switch(true){
+    case (b>32 && b<40):
+        console.log('D');
+        break;
+    case (b>39 && b<50):
+        console.log('C');
+        break;
+    case (b>49 && b<60):
+        console.log('B');
+         break;
+    case (b>59 && b<70):
+        console.log('A-');
+        break;
+    case (b>69 && b<80):
+        console.log('A');
+        break;
+    case (b>79 && b<101):
+        console.log('A+');
+        break;
+    default:console.log('fail');
+}
+*/
+/*
+for(let i=0; i<51; i++){
+    console.log(`${i*2}`);
+}
+*/
+/*while loop; when condition are 3*/
+/*
+while(a<22){
+    a++;
+    console.log(a);
+}
+*/
+/* do while loop*/
+/*
+do{
+    a++;
+    console.log(a);
+}while(a<20);
+
+for(let i=1; i<20; i++){
+    a = a*i;
+}
+console.log(a);*/
+/*
+for(let i=1; i<20; i++){
+    a = a*i;
+    console.log(a);
+}
+*/
